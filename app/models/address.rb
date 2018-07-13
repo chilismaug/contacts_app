8b076e179d2554e3c7236db1ede5f4d3fb2eb0da
charles ross <charles.ross.oh@gmail.com>
2018-07-13 14:34:45 -0400

    add field edits to model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,18 @@
 class Address < ActiveRecord::Base
+	validates_presence_of :phone
+	validates_presence_of :lastname
+	validates_presence_of :firstname
+	before_validation :preval
+	private
+	  def preval
+	    if self.lastname
+	      self.lastname = self.lastname.strip
+	    end
+	    if self.firstname
+	      self.firstname = self.firstname.strip
+	    end
+		if self.phone
+	      self.phone = self.phone.strip
+	    end    
+	end
 end
