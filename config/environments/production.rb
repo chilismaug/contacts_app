d8fd9c7752d4c3ac796522a6cddbc95e5e4fc887
charles ross <charles.ross.oh@gmail.com>
2018-07-16 15:59:15 -0400

    typo in actionmailer cfg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ end
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'heroku.com,
+  :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
